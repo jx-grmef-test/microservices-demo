334b76c97c8deeff7a2ac529552e661ba646a15b
grmef <35537909+grmef@users.noreply.github.com>
2020-08-03 11:11:47 -0400

    Build Adservice Image

--- Jenkins_files/pipeline_docker/test.groovy
+++ Jenkins_files/pipeline_docker/test.groovy
@@ -1,57 +1,49 @@
 node {
-    environment {
-        registry = "jx-grmef-test/microservices-demo"
-        registryCredential = 'jx-pipeline-git-github-github'
-    }
-    agent any
 
     def app
-    stages {
-        stage('GitCheckout') {
-            // Cloning project microservices-demo
-            // checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'jx-pipeline-git-github-github', url: 'https://github.com/jx-grmef-test/microservices-demo.git']]])
+    stage('GitCheckout') {
+        // Cloning project microservices-demo
+        // checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'jx-pipeline-git-github-github', url: 'https://github.com/jx-grmef-test/microservices-demo.git']]])
 
-            checkout scm
+        checkout scm
 
-            sh 'pwd && ls -l && ls -l release'
+        sh 'pwd && ls -l && ls -l release'
+    }
+    stage('Initialize') {
+        def dockerHome = tool 'Docker-19'
+        env.PATH = "${dockerHome}/bin:${env.PATH}"
+
+        sh 'docker -v'
+
+    }
+    stage('Build Adservice image') {
+        /* This builds the actual image; synonymous to
+         * docker build on the command line */
+        dir("src/adservice") {
+            sh "pwd"
+            app = docker.build("microservices-demo/src/adservice")
         }
-        stage('Initialize') {
-            def dockerHome = tool 'Docker-default'
-            env.PATH = "${dockerHome}/bin:${env.PATH}"
 
-            sh 'docker -v'
+        stage('Test Adservice image') {
+            /* We test our image with a simple smoke test:
+             * Run a curl inside the newly-build Docker image */
 
+            sh "echo $PATH"
+            docker.image("microservices-demo/src/adservice:latest").inside("--entrypoint='/app/build/install/hipstershop/bin/AdService'") { c ->
+                //sh 'curl http://localhost:9555 || exit 1'
+                sh 'cat /app/build/install/hipstershop/bin/AdService'
+                sh 'echo "Tests passed"'
+            }
         }
-        stage('Build Adservice image') {
-            /* This builds the actual image; synonymous to
-             * docker build on the command line */
-
-            app = docker.build( registry + "/src/adservice/")
-            //steps {
-            //    script {
-            //        //docker.build registry + ":$BUILD_NUMBER"
-            //        docker.build("jx-grmef-test/microservices-demo/src/adservice/")
-            //    }
-            //}
-            //stage('Test image') {
-            //    /* We test our image with a simple smoke test:
-            //     * Run a curl inside the newly-build Docker image */
-
-            //    app.inside {
-            //        sh 'curl http://localhost:9555 || exit 1'
-            //        sh 'echo "Tests passed"'
-            //    }
-            //}
-            //stage('Push image') {
-            //    /* Finally, we'll push the image with two tags:
-            //     * First, the incremental build number from Jenkins
-            //     * Second, the 'latest' tag.
-            //     * Pushing multiple tags is cheap, as all the layers are reused. */
-            //    docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
-            //        app.push("${env.BUILD_NUMBER}")
-            //        app.push("latest")
-            //    }
-            //}
+        stage('Push Adservice image to Nexus') {
+            /* Finally, we'll push the image with two tags:
+             * First, the incremental build number from Jenkins
+             * Second, the 'latest' tag.
+             * Pushing multiple tags is cheap, as all the layers are reused. */
+            docker.withRegistry('http://nexus.apps.meflab.xyz/repository/microservices-demo/', 'nexus-password') {
+                app.push("${env.BUILD_NUMBER}")
+                app.push("latest")
+            }
         }
     }
 }
