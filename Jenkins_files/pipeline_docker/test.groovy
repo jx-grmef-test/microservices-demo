2a07a804c5f309ca53f39ce72dcb9858cab3b2d2
grmef <35537909+grmef@users.noreply.github.com>
2020-08-03 13:52:42 -0400

    Build all Images

--- Jenkins_files/pipeline_docker/test.groovy
+++ Jenkins_files/pipeline_docker/test.groovy
@@ -16,34 +16,151 @@ node {
         sh 'docker -v'
 
     }
-    stage('Build Adservice image') {
+    stage('Build image') {
         /* This builds the actual image; synonymous to
          * docker build on the command line */
         dir("src/adservice") {
             sh "pwd"
-            app = docker.build("microservices-demo/src/adservice")
+            app = docker.build("microservices-demo/image/adservice")
         }
 
-        stage('Test Adservice image') {
+        dir("src/cartservice") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/cartservice")
+        }
+
+        dir("src/checkoutservice") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/checkoutservice")
+        }
+
+        dir("src/currencyservice") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/currencyservice")
+        }
+
+        dir("src/emailservice") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/emailservice")
+        }
+
+        dir("src/frontend") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/frontend")
+        }
+
+        dir("src/loadgenerator") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/loadgenerator")
+        }
+
+        dir("src/paymentservice") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/paymentservice")
+        }
+
+        dir("src/productcatalogservice") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/productcatalogservice")
+        }
+
+        dir("src/recommendationservice") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/recommendationservice")
+        }
+
+        dir("src/shippingservice") {
+            sh "pwd"
+            app = docker.build("microservices-demo/image/shippingservice")
+        }
+
+        stage('Test image') {
             /* We test our image with a simple smoke test:
              * Run a curl inside the newly-build Docker image */
 
             sh "echo $PATH"
-            docker.image("microservices-demo/src/adservice:latest").inside("--entrypoint='/app/build/install/hipstershop/bin/AdService'") { c ->
+            docker.image("microservices-demo/image/adservice:latest").inside("--entrypoint='/app/build/install/hipstershop/bin/AdService'") { c ->
                 //sh 'curl http://localhost:9555 || exit 1'
                 sh 'cat /app/build/install/hipstershop/bin/AdService'
                 sh 'echo "Tests passed"'
             }
+
+            docker.image("microservices-demo/image/cartservice:latest").inside("--entrypoint=\"./cartservice\", \"start\"") { c ->
+                //sh 'curl http://localhost:7070 || exit 1'
+                sh 'cat ./cartservice'
+                sh 'echo "Tests passed"'
+            }
+
+            docker.image("microservices-demo/image/checkoutservice:latest").inside("--entrypoint='/checkoutservice'") { c ->
+                //sh 'curl http://localhost:5050 || exit 1'
+                sh 'cat /checkoutservice'
+                sh 'echo "Tests passed"'
+            }
+
+            docker.image("microservices-demo/image/currencyservice:latest").inside("--entrypoint=\"node\", \"server.js") { c ->
+                //sh 'curl http://localhost:7000 || exit 1'
+                sh 'which node'
+                sh 'find / -type f -name server.js'
+                sh 'echo "Currency Service Test passed"'
+            }
+
+            docker.image("microservices-demo/image/emailservice:latest").inside("--entrypoint=\"python\", \"email_server.py\"") { c ->
+                //sh 'curl http://localhost:5000 || exit 1'
+                sh 'which python'
+                sh 'find / -type f -name email_server.py'
+                sh 'echo "Tests passed"'
+            }
+
+            docker.image("microservices-demo/image/frontend:latest").inside("--entrypoint='/frontend/server'") { c ->
+                //sh 'curl http://localhost:8080 || exit 1'
+                sh 'cat /frontend/server'
+                sh 'echo "Tests passed"'
+            }
+
+            docker.image("microservices-demo/image/loadgenerator:latest").inside("--entrypoint='./loadgen.sh'") { c ->
+                //sh 'curl http://localhost:80 || exit 1'
+                sh 'cat ./loadgen.sh'
+                sh 'echo "Tests passed"'
+            }
+
+            docker.image("microservices-demo/image/paymentservice:latest").inside("--entrypoint='\"node\", \"index.js\"'") { c ->
+                //sh 'curl http://localhost:50051 || exit 1'
+                sh 'which node'
+                sh 'find / -type f -name index.js'
+                sh 'echo "Tests passed"'
+            }
+
+            docker.image("microservices-demo/image/productcatalogservice:latest").inside("--entrypoint='/productcatalogservice/server'") { c ->
+                //sh 'curl http://localhost:3550 || exit 1'
+                sh 'cat /productcatalogservice/server'
+                sh 'echo "Tests passed"'
+            }
+
+            docker.image("microservices-demo/image/recommendationservice:latest").inside("--entrypoint='/recommendationservice/recommendation_server.py'") { c ->
+                //sh 'curl http://localhost:8080 || exit 1'
+                sh 'cat /recommendationservice/recommendation_server.py'
+                sh 'echo "Tests passed"'
+            }
+
+            docker.image("microservices-demo/image/shippingservice:latest").inside("--entrypoint='/shippingservice'") { c ->
+                //sh 'curl http://localhost:50051 || exit 1'
+                sh 'cat /shippingservice'
+                sh 'echo "Tests passed"'
+            }
         }
-        stage('Push Adservice image to Nexus') {
+        stage('Push image to Nexus') {
             /* Finally, we'll push the image with two tags:
              * First, the incremental build number from Jenkins
              * Second, the 'latest' tag.
              * Pushing multiple tags is cheap, as all the layers are reused. */
-            docker.withRegistry('http://nexus.apps.meflab.xyz/repository/microservices-demo/', 'nexus-password') {
+            docker.withRegistry('http://nexus-docker.apps.meflab.xyz/repository/microservices-demo/', 'nexus-password') {
                 app.push("${env.BUILD_NUMBER}")
                 app.push("latest")
             }
+        }
+        stage('Remove Unused docker image') {
+            //sh "docker rmi $registry:$BUILD_NUMBER"
+            //sh 'docker rmi $(docker images --filter=reference="http://nexus-docker.apps.meflab.xyz/repository/microservices-demo/shippingservice*" -q)'
         }
     }
 }
