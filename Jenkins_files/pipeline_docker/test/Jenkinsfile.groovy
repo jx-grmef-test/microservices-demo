4944feb8a841da3f45e6ececd13d6663c1575b10
grmef <35537909+grmef@users.noreply.github.com>
2020-08-04 11:36:07 -0400

    Test

--- Jenkins_files/pipeline_docker/test/Jenkinsfile.groovy
+++ Jenkins_files/pipeline_docker/test/Jenkinsfile.groovy
@@ -51,9 +51,9 @@ pipeline {
                         //imageName = "adservice"
                         imageName   = sh(returnStdout: true, script: "pwd | awk -F \"/\" '{print \$NF}'").trim()
 
-                        echo "######################################\n" +
+                        echo "####################################\n" +
                             ("# Building ${imageName} image ${devTag} #\n" as java.lang.CharSequence) +
-                            ("######################################" as java.lang.CharSequence)
+                            ("####################################" as java.lang.CharSequence)
 
                         app = docker.build("${projectName}/image/${imageName}")
                     }
@@ -64,9 +64,9 @@ pipeline {
             /* We test our image with a simple smoke test:
              * Run a curl inside the newly-build Docker image */
             steps {
-                echo "#####################################\n" +
+                echo "###################################\n" +
                     ("# Testing ${imageName} image ${devTag} #\n" as java.lang.CharSequence) +
-                    ("#####################################" as java.lang.CharSequence)
+                    ("###################################" as java.lang.CharSequence)
                 sh "echo $PATH"
                 script {
                     docker.image("${projectName}/image/adservice:latest").inside("--entrypoint=''") { c ->
@@ -76,9 +76,9 @@ pipeline {
                         sh 'echo "Tests passed"'
                     }
                 }
-                echo "#####################################\n" +
+                echo "###################################\n" +
                     ("# Pushing ${imageName} image ${devTag} #\n" as java.lang.CharSequence) +
-                    ("#####################################" as java.lang.CharSequence)
+                    ("###################################" as java.lang.CharSequence)
                 script {
                     docker.withRegistry("${nexusRegistry}", "${nexusPassword}") {
                         app.push("${env.BUILD_NUMBER}")
@@ -89,9 +89,9 @@ pipeline {
         }
         stage ('Clean images') {
             steps {
-                echo "#############################\n" +
+                echo "###########################\n" +
                     ("# Cleaning up Docker Images #\n" as java.lang.CharSequence) +
-                    ("#############################" as java.lang.CharSequence)
+                    ("###########################" as java.lang.CharSequence)
 
                 echo "### Dangling all Containers, Images, and Volumes"
                 //sh 'docker system prune -af --volumes'
